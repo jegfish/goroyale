99aa178c1fb068a87f3dc6ee94de24bfb8245f69
Altarrel <altarrel7@gmail.com>
2018-06-18 15:56:39 -0400

    Removed 'Get' from endpoint methods

--- endpoints.go
+++ endpoints.go
@@ -6,9 +6,9 @@ import (
 	"strings"
 )
 
-// GetAPIVersion requests the current version of the API.
+// APIVersion requests the current version of the API.
 // https://docs.royaleapi.com/#/endpoints/version
-func (c *Client) GetAPIVersion() (ver string, err error) {
+func (c *Client) APIVersion() (ver string, err error) {
 	bytes, err := c.get("/version", url.Values{})
 	if err != nil {
 		return
@@ -17,9 +17,9 @@ func (c *Client) GetAPIVersion() (ver string, err error) {
 	return
 }
 
-// GetConstants returns constants from the API.
+// Constants returns constants from the API.
 // https://docs.royaleapi.com/#/endpoints/constants
-func (c *Client) GetConstants(params url.Values) (constants Constants, err error) {
+func (c *Client) Constants(params url.Values) (constants Constants, err error) {
 	var b []byte
 	path := "/constants"
 	if b, err = c.get(path, params); err == nil {
@@ -28,9 +28,9 @@ func (c *Client) GetConstants(params url.Values) (constants Constants, err error
 	return
 }
 
-// GetPlayer retrieves a player by their tag.
+// Player retrieves a player by their tag.
 // https://docs.royaleapi.com/#/endpoints/player
-func (c *Client) GetPlayer(tag string, params url.Values) (player Player, err error) {
+func (c *Client) Player(tag string, params url.Values) (player Player, err error) {
 	var b []byte
 	path := "/player/" + tag
 	if b, err = c.get(path, params); err == nil {
@@ -39,10 +39,10 @@ func (c *Client) GetPlayer(tag string, params url.Values) (player Player, err er
 	return
 }
 
-// GetPlayers works like GetPlayer but can return multiple players.
+// Players works like Player but can return multiple players.
 // The API asks that you don't include more than 7 tags in this request.
 // https://docs.royaleapi.com/#/endpoints/player?id=multiple-players
-func (c *Client) GetPlayers(tags []string, params url.Values) (players []Player, err error) {
+func (c *Client) Players(tags []string, params url.Values) (players []Player, err error) {
 	var b []byte
 	path := "/player/" + strings.Join(tags, ",")
 	if b, err = c.get(path, params); err == nil {
@@ -51,9 +51,9 @@ func (c *Client) GetPlayers(tags []string, params url.Values) (players []Player,
 	return
 }
 
-// GetPlayerBattles gets battles a player participated in.
+// PlayerBattles s battles a player participated in.
 // https://docs.royaleapi.com/#/endpoints/player_battles
-func (c *Client) GetPlayerBattles(tag string, params url.Values) (battles []Battle, err error) {
+func (c *Client) PlayerBattles(tag string, params url.Values) (battles []Battle, err error) {
 	var b []byte
 	path := "/player/" + tag + "/battles"
 	if b, err = c.get(path, params); err == nil {
@@ -62,9 +62,9 @@ func (c *Client) GetPlayerBattles(tag string, params url.Values) (battles []Batt
 	return
 }
 
-// GetPlayersBattles works like GetPlayerBattles but can return battles from multiple players.
+// PlayersBattles works like PlayerBattles but can return battles from multiple players.
 // https://docs.royaleapi.com/#/endpoints/player_battles?id=multiple-tags
-func (c *Client) GetPlayersBattles(tags []string, params url.Values) (battles [][]Battle, err error) {
+func (c *Client) PlayersBattles(tags []string, params url.Values) (battles [][]Battle, err error) {
 	var b []byte
 	path := "/player/" + strings.Join(tags, ",") + "/battles"
 	if b, err = c.get(path, params); err == nil {
@@ -73,9 +73,9 @@ func (c *Client) GetPlayersBattles(tags []string, params url.Values) (battles []
 	return
 }
 
-// GetPlayerChests gets a player's upcoming chests.
+// PlayerChests s a player's upcoming chests.
 // https://docs.royaleapi.com/#/endpoints/player_chests
-func (c *Client) GetPlayerChests(tag string, params url.Values) (chests PlayerChests, err error) {
+func (c *Client) PlayerChests(tag string, params url.Values) (chests PlayerChests, err error) {
 	var b []byte
 	path := "/player/" + tag + "/chests"
 	if b, err = c.get(path, params); err == nil {
@@ -84,9 +84,9 @@ func (c *Client) GetPlayerChests(tag string, params url.Values) (chests PlayerCh
 	return
 }
 
-// GetPlayersChests works like GetPlayerChests but can return chests for multiple players.
+// PlayersChests works like PlayerChests but can return chests for multiple players.
 // https://docs.royaleapi.com/#/endpoints/player_chests?id=multiple-players
-func (c *Client) GetPlayersChests(tags []string, params url.Values) (chests []PlayerChests, err error) {
+func (c *Client) PlayersChests(tags []string, params url.Values) (chests []PlayerChests, err error) {
 	var b []byte
 	path := "/player/" + strings.Join(tags, ",") + "/chests"
 	if b, err = c.get(path, params); err == nil {
@@ -106,9 +106,9 @@ func (c *Client) ClanSearch(params url.Values) (clans []ClanSearch, err error) {
 	return
 }
 
-// GetClan returns info about a specific clan.
+// Clan returns info about a specific clan.
 // https://docs.royaleapi.com/#/endpoints/clan
-func (c *Client) GetClan(tag string, params url.Values) (clan Clan, err error) {
+func (c *Client) Clan(tag string, params url.Values) (clan Clan, err error) {
 	var b []byte
 	path := "/clan/" + tag
 	if b, err = c.get(path, params); err == nil {
@@ -117,9 +117,9 @@ func (c *Client) GetClan(tag string, params url.Values) (clan Clan, err error) {
 	return
 }
 
-// GetClans works like GetClan but can return multiple clans.
+// Clans works like Clan but can return multiple clans.
 // https://docs.royaleapi.com/#/endpoints/clan?id=multiple-clans
-func (c *Client) GetClans(tags []string, params url.Values) (clans []Clan, err error) {
+func (c *Client) Clans(tags []string, params url.Values) (clans []Clan, err error) {
 	var b []byte
 	path := "/clan/" + strings.Join(tags, ",")
 	if b, err = c.get(path, params); err == nil {
@@ -128,9 +128,9 @@ func (c *Client) GetClans(tags []string, params url.Values) (clans []Clan, err e
 	return
 }
 
-// GetClanBattles returns battles played by people in the specified clan.
+// ClanBattles returns battles played by people in the specified clan.
 // https://docs.royaleapi.com/#/endpoints/clan_battles
-func (c *Client) GetClanBattles(tag string, params url.Values) (battles []Battle, err error) {
+func (c *Client) ClanBattles(tag string, params url.Values) (battles []Battle, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/battles"
 	if b, err = c.get(path, params); err == nil {
@@ -139,9 +139,9 @@ func (c *Client) GetClanBattles(tag string, params url.Values) (battles []Battle
 	return
 }
 
-// GetClanWar returns data about the current clan war.
+// ClanWar returns data about the current clan war.
 // https://docs.royaleapi.com/#/endpoints/clan_war
-func (c *Client) GetClanWar(tag string, params url.Values) (war ClanWar, err error) {
+func (c *Client) ClanWar(tag string, params url.Values) (war ClanWar, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/war"
 	if b, err = c.get(path, params); err == nil {
@@ -150,9 +150,9 @@ func (c *Client) GetClanWar(tag string, params url.Values) (war ClanWar, err err
 	return
 }
 
-// GetClanWarLog returns data about past clan wars.
+// ClanWarLog returns data about past clan wars.
 // https://docs.royaleapi.com/#/endpoints/clan_warlog
-func (c *Client) GetClanWarLog(tag string, params url.Values) (warlog []ClanWarLogEntry, err error) {
+func (c *Client) ClanWarLog(tag string, params url.Values) (warlog []ClanWarLogEntry, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/warlog"
 	if b, err = c.get(path, params); err == nil {
@@ -161,10 +161,10 @@ func (c *Client) GetClanWarLog(tag string, params url.Values) (warlog []ClanWarL
 	return
 }
 
-// GetClanHistory returns a time series of member stats.
+// ClanHistory returns a time series of member stats.
 // This will only work with clans that have enabled stat tracking.
 // https://docs.royaleapi.com/#/endpoints/clan_history
-func (c *Client) GetClanHistory(tag string, params url.Values) (history []ClanHistoryEntry, err error) {
+func (c *Client) ClanHistory(tag string, params url.Values) (history []ClanHistoryEntry, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/history"
 	if b, err = c.get(path, params); err == nil {
@@ -173,9 +173,9 @@ func (c *Client) GetClanHistory(tag string, params url.Values) (history []ClanHi
 	return
 }
 
-// GetClanWeeklyHistory works like GetClanHistory but returns weekly stats.
+// ClanWeeklyHistory works like ClanHistory but returns weekly stats.
 // https://docs.royaleapi.com/#/endpoints/clan_history_weekly
-func (c *Client) GetClanWeeklyHistory(tag string, params url.Values) (history []ClanHistoryEntry, err error) {
+func (c *Client) ClanWeeklyHistory(tag string, params url.Values) (history []ClanHistoryEntry, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/history/weekly"
 	if b, err = c.get(path, params); err == nil {
@@ -184,9 +184,9 @@ func (c *Client) GetClanWeeklyHistory(tag string, params url.Values) (history []
 	return
 }
 
-// GetClanTracking returns basic data on whether a clan is tracked.
+// ClanTracking returns basic data on whether a clan is tracked.
 // https://docs.royaleapi.com/#/endpoints/clan_tracking
-func (c *Client) GetClanTracking(tag string, params url.Values) (tracking ClanTracking, err error) {
+func (c *Client) ClanTracking(tag string, params url.Values) (tracking ClanTracking, err error) {
 	var b []byte
 	path := "/clan/" + tag + "/tracking"
 	if b, err = c.get(path, params); err == nil {
@@ -195,9 +195,9 @@ func (c *Client) GetClanTracking(tag string, params url.Values) (tracking ClanTr
 	return
 }
 
-// GetOpenTournaments returns a slice of open tournaments.
+// OpenTournaments returns a slice of open tournaments.
 // https://docs.royaleapi.com/#/endpoints/tournaments_open
-func (c *Client) GetOpenTournaments(params url.Values) (tournaments []Tournament, err error) {
+func (c *Client) OpenTournaments(params url.Values) (tournaments []Tournament, err error) {
 	var b []byte
 	path := "/tournaments/open"
 	if b, err = c.get(path, params); err == nil {
@@ -206,9 +206,9 @@ func (c *Client) GetOpenTournaments(params url.Values) (tournaments []Tournament
 	return
 }
 
-// GetKnownTournaments returns a slice of tournaments people have searched for.
+// KnownTournaments returns a slice of tournaments people have searched for.
 // https://docs.royaleapi.com/#/endpoints/tournaments_known
-func (c *Client) GetKnownTournaments(params url.Values) (tournaments []Tournament, err error) {
+func (c *Client) KnownTournaments(params url.Values) (tournaments []Tournament, err error) {
 	var b []byte
 	path := "/tournaments/known"
 	if b, err = c.get(path, params); err == nil {
@@ -227,8 +227,8 @@ func (c *Client) Get1kTournaments(params url.Values) (tournaments []Tournament1k
 	return
 }
 
-// GetPrepTournaments returns a slice of tournaments that have a Status of "inPreparation".
-func (c *Client) GetPrepTournaments(params url.Values) (tournaments []PrepTournament, err error) {
+// PrepTournaments returns a slice of tournaments that have a Status of "inPreparation".
+func (c *Client) PrepTournaments(params url.Values) (tournaments []PrepTournament, err error) {
 	var b []byte
 	path := "/tournaments/prep"
 	if b, err = c.get(path, params); err == nil {
@@ -248,9 +248,9 @@ func (c *Client) TournamentSearch(params url.Values) (tournaments []SearchedTour
 	return
 }
 
-// GetTournament returns the specified Tournament by tag.
+// Tournament returns the specified Tournament by tag.
 // https://docs.royaleapi.com/#/endpoints/tournaments
-func (c *Client) GetTournament(tag string, params url.Values) (tournament SpecificTournament, err error) {
+func (c *Client) Tournament(tag string, params url.Values) (tournament SpecificTournament, err error) {
 	var b []byte
 	path := "/tournaments/" + tag
 	if b, err = c.get(path, params); err == nil {
@@ -259,9 +259,9 @@ func (c *Client) GetTournament(tag string, params url.Values) (tournament Specif
 	return
 }
 
-// GetTournaments works like GetTournament but can return multiple SpecificTournaments.
+// Tournaments works like Tournament but can return multiple SpecificTournaments.
 // https://docs.royaleapi.com/#/endpoints/tournaments?id=multiple-tournaments
-func (c *Client) GetTournaments(tags []string, params url.Values) (tournaments []SpecificTournament, err error) {
+func (c *Client) Tournaments(tags []string, params url.Values) (tournaments []SpecificTournament, err error) {
 	var b []byte
 	path := "/tournaments/" + strings.Join(tags, ",")
 	if b, err = c.get(path, params); err == nil {
@@ -270,9 +270,9 @@ func (c *Client) GetTournaments(tags []string, params url.Values) (tournaments [
 	return
 }
 
-// GetTopClans returns the top 200 clans of a location/global leaderboard.
+// TopClans returns the top 200 clans of a location/global leaderboard.
 // https://docs.royaleapi.com/#/endpoints/top_clans
-func (c *Client) GetTopClans(location string, params url.Values) (topClans []TopClan, err error) {
+func (c *Client) TopClans(location string, params url.Values) (topClans []TopClan, err error) {
 	var b []byte
 	path := "/top/clans/" + location
 	if b, err = c.get(path, params); err == nil {
@@ -281,9 +281,9 @@ func (c *Client) GetTopClans(location string, params url.Values) (topClans []Top
 	return
 }
 
-// GetTopPlayers returns the top 200 players of a location/global leaderboard.
+// TopPlayers returns the top 200 players of a location/global leaderboard.
 // https://docs.royaleapi.com/#/endpoints/top_players
-func (c *Client) GetTopPlayers(location string, params url.Values) (topPlayers []TopPlayer, err error) {
+func (c *Client) TopPlayers(location string, params url.Values) (topPlayers []TopPlayer, err error) {
 	var b []byte
 	path := "/top/players/" + location
 	if b, err = c.get(path, params); err == nil {
@@ -292,9 +292,9 @@ func (c *Client) GetTopPlayers(location string, params url.Values) (topPlayers [
 	return
 }
 
-// GetPopularClans returns stats on how often a clan's data has been requested from the API.
+// PopularClans returns stats on how often a clan's data has been requested from the API.
 // https://docs.royaleapi.com/#/endpoints/popular_clans
-func (c *Client) GetPopularClans(params url.Values) (popularClans []PopularClan, err error) {
+func (c *Client) PopularClans(params url.Values) (popularClans []PopularClan, err error) {
 	var b []byte
 	path := "/popular/clans"
 	if b, err = c.get(path, params); err == nil {
@@ -303,9 +303,9 @@ func (c *Client) GetPopularClans(params url.Values) (popularClans []PopularClan,
 	return
 }
 
-// GetPopularPlayers returns stats on how often a player's data has been request from the API.
+// PopularPlayers returns stats on how often a player's data has been request from the API.
 // https://docs.royaleapi.com/#/endpoints/popular_players
-func (c *Client) GetPopularPlayers(params url.Values) (popularPlayers []PopularPlayer, err error) {
+func (c *Client) PopularPlayers(params url.Values) (popularPlayers []PopularPlayer, err error) {
 	var b []byte
 	path := "/popular/players"
 	if b, err = c.get(path, params); err == nil {
@@ -314,9 +314,9 @@ func (c *Client) GetPopularPlayers(params url.Values) (popularPlayers []PopularP
 	return
 }
 
-// GetPopularTournaments returns stats on how often a tournament's data has been request from the API.
+// PopularTournaments returns stats on how often a tournament's data has been request from the API.
 // https://docs.royaleapi.com/#/endpoints/popular_tournaments
-func (c *Client) GetPopularTournaments(params url.Values) (popularTournaments []PopularTournament, err error) {
+func (c *Client) PopularTournaments(params url.Values) (popularTournaments []PopularTournament, err error) {
 	var b []byte
 	path := "/popular/tournaments"
 	if b, err = c.get(path, params); err == nil {
@@ -325,9 +325,9 @@ func (c *Client) GetPopularTournaments(params url.Values) (popularTournaments []
 	return
 }
 
-// GetPopularDecks returns stats on how often a deck's data has been requested from the API.
+// PopularDecks returns stats on how often a deck's data has been requested from the API.
 // https://docs.royaleapi.com/#/endpoints/popular_decks
-func (c *Client) GetPopularDecks(params url.Values) (popularDecks []PopularDeck, err error) {
+func (c *Client) PopularDecks(params url.Values) (popularDecks []PopularDeck, err error) {
 	var b []byte
 	path := "/popular/decks"
 	if b, err = c.get(path, params); err == nil {
@@ -336,9 +336,9 @@ func (c *Client) GetPopularDecks(params url.Values) (popularDecks []PopularDeck,
 	return
 }
 
-// GetAPIKeyStats returns information about the currently authenticated token.
+// APIKeyStats returns information about the currently authenticated token.
 // https://docs.royaleapi.com/#/endpoints/auth_stats
-func (c *Client) GetAPIKeyStats(params url.Values) (keyStats APIKeyStats, err error) {
+func (c *Client) APIKeyStats(params url.Values) (keyStats APIKeyStats, err error) {
 	var b []byte
 	path := "/auth/stats"
 	if b, err = c.get(path, params); err == nil {
@@ -347,10 +347,10 @@ func (c *Client) GetAPIKeyStats(params url.Values) (keyStats APIKeyStats, err er
 	return
 }
 
-// GetEndpoints returns all the available endpoints for the API.
+// Endpoints returns all the available endpoints for the API.
 // It does not have any special incorporation with this wrapper and is simply included for completion's sake.
 // https://docs.royaleapi.com/#/endpoints/endpoints
-func (c *Client) GetEndpoints(params url.Values) (endpoints []string, err error) {
+func (c *Client) Endpoints(params url.Values) (endpoints []string, err error) {
 	var b []byte
 	path := "/endpoints"
 	if b, err = c.get(path, params); err == nil {
